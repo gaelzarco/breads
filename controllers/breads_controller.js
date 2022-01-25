fb5b183172ae041e77e129c17aa08da5eaeb55e6
gaelzarco <gaelzarco@yahoo.com>
2022-01-24 21:21:51 -0800

    mongoose issues. stuck on BreadCRUD Helper Methods Pt1

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,15 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
-
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index', {
-        breads: Bread,
-        title: 'Index Page'
-    })
+  console.log("index route works")
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
 
 // NEW
@@ -39,17 +42,18 @@ breads.get('/:arrayIndex', (req, res) => {
 
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
+
 
 // UPDATE
 breads.put('/:arrayIndex', (req, res) => {

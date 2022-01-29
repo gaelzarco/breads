2c052be3aeb11b102648d2ffaccdc91f299f26ce
gaelzarco <gaelzarco@yahoo.com>
2022-01-29 11:01:57 -0800

    added breadCRUD populate

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -27,17 +27,22 @@ breads.get('/new', (req, res) => {
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  Bread.findById(req.params.id) 
-    .then(foundBread => { 
-      res.render('edit', {
-        bread: foundBread 
-      })
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
     })
 })
 
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
+  .populate('baker')
       .then(foundBread => {
         foundBread.getBakedBy() 
         res.render('show', {

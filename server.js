c4e21398f8f68fdc08203d06032f3550163fd353
gaelzarco <gaelzarco@yahoo.com>
2022-02-05 11:33:59 -0800

    final debug hoepfully

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+    () => { console.log('munchin on',process.env.MONGO_URI) }
   )
 
 // ROUTES

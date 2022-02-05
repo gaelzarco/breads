9544cebcc487481e842241e776882f04ffe4d6cc
gaelzarco <gaelzarco@yahoo.com>
2022-02-05 11:04:00 -0800

    debugging heroku

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-  )
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // ROUTES
 app.get('/', (req, res) => {
